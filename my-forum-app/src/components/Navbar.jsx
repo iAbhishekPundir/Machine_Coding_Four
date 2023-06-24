a6473294385ef680736754b42a973531fd93f8f2
Abhishek <pundirabhishek987@gmail.com>
2023-06-24 22:09:40 +0530

    feat: Navbar Component

---
+++ my-forum-app/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Forum, Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Navbar = () => {
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          My Forum
+        </Typography>
+        <Forum sx={{ display: { xs: "block", sm: "none" } }} />
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
